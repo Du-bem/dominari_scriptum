ec59bdab2d62269a25df0fc6867e5e50397422ff
NeonFeline <maciejlachut77@gmail.com>
2025-03-22 19:58:47 +0100

    merged

--- user-interface/src/App.vue
+++ user-interface/src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import Plot3d from "./components/3d.vue";
+import rightComp from "./components/right_comp.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <rightComp />
 </template>
 
 <style scoped>

e054df1582828fa05984d61fa46e4304706797be
dmigwi <migwindungu0@gmail.com>
2025-03-23 02:18:51 +0100

    Added the actual implementation to handle all the endpoints

--- main_node/server/server.go
+++ main_node/server/server.go
@@ -1,47 +1,137 @@
 package server
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Du-bem/dominari_scriptum/main_node/types"
+	"github.com/btnguyen2k/consu/checksum"
 	"github.com/gorilla/mux"
 )
 
 type serverAPI types.ServerAPI
 
-func (s serverAPI) handleAccBalance(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UI API data handler is not yet implemented!")
-}
-func (s serverAPI) handleAccTx(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UI API data handler is not yet implemented!")
-}
-func (s serverAPI) handleAccTxs(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UI API data handler is not yet implemented!")
-}
-func (s serverAPI) handleListSatData(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UI API data handler is not yet implemented!")
-}
-func (s *serverAPI) handleSearchSatData(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UI API data handler is not yet implemented!")
+// handleAccountBalance handles serving the account balance UI request
+func (s serverAPI) handleAccountBalance(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(" AccountBalance is Serving: ", r.URL.String())
+
+	fmt.Fprintf(w, `{"balance": "%d"}`, s.GetBalance())
 }
 
-func (s *serverAPI) handleSatelliteData(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Satellite API data handler is not yet implemented!")
+// handleAccountTx handles serving the account tx identified by the tx id provided
+func (s serverAPI) handleAccountTx(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(" AccountTx is Serving: ", r.URL.String())
+
+	vars := mux.Vars(r)
+	txID, ok := vars["key"]
+	if ok {
+		data, err := s.GetTransaction(txID)
+		if err != nil {
+			jData, _ := json.Marshal(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jData)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, `{"error" : "%s"}`, err)
+		}
+	}
 }
 
+// handleAccountTxs handles serving the account txs associated with the current account
+func (s serverAPI) handleAccountTxs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(" AccountTxs is Serving: ", r.URL.String())
+
+	data, err := s.GetTransactions()
+	if err != nil {
+		jData, _ := json.Marshal(data)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jData)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, `{"error" : "%s"}`, err)
+	}
+}
+
+// handleListSatelliteData handles serving the satellite data stored locally
+func (s serverAPI) handleListSatelliteData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(" ListSatelliteData is Serving: ", r.URL.String())
+
+	vars := mux.Vars(r)
+	val, ok := vars["offset"]
+	if ok {
+		offset, _ := strconv.Atoi(val)
+		data, err := s.List(offset)
+		if err != nil {
+			jData, _ := json.Marshal(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jData)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, `{"error" : "%s"}`, err)
+		}
+	}
+}
+
+// handleSearchSatelliteByID handles searching the locally stored satellite data by id
+func (s *serverAPI) handleSearchSatelliteByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(" SearchSatelliteByID is Serving: ", r.URL.String())
+
+	vars := mux.Vars(r)
+	txID, ok := vars["txid"]
+	if ok {
+		id, _ := strconv.Atoi(txID)
+		data, err := s.SearchByID(id)
+		if err != nil {
+			jData, _ := json.Marshal(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jData)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, `{"error" : "%s"}`, err)
+		}
+	}
+}
+
+// handleSatelliteDataStore generates a SHA256 checksum of the data before storing
+// in the database.
+func (s *serverAPI) handleSatelliteDataStore(w http.ResponseWriter, r *http.Request) {
+	var data types.SatelliteRequestData
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"error" : "%s"}`, err)
+		return
+	}
+
+	checkSumStr := hex.EncodeToString(checksum.Sha256Checksum(data))
+	_, err = s.Insert(checkSumStr, &data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"error" : "%s"}`, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// RunServer takes a server API interface that exposes all the methods needed to
+// serve all the http requests.
 func RunServer(val types.ServerAPI) {
 	s := serverAPI(val)
 	router := mux.NewRouter()
-	router.HandleFunc("/ui/balance", s.handleAccBalance).Methods("GET")
-	router.HandleFunc("/ui/tx", s.handleAccTx).Methods("GET")
-	router.HandleFunc("/ui/txs", s.handleAccTxs).Methods("GET")
-	router.HandleFunc("/ui/list", s.handleListSatData).Methods("GET")
-	router.HandleFunc("/ui/search", s.handleSearchSatData).Methods("GET")
+	router.HandleFunc("/ui/balance", s.handleAccountBalance).Methods("GET")
+	router.HandleFunc("/ui/tx/{key}", s.handleAccountTx).Methods("GET")
+	router.HandleFunc("/ui/txs", s.handleAccountTxs).Methods("GET")
+	router.HandleFunc("/ui/list/{offset:[0-9]+}", s.handleListSatelliteData).Methods("GET")
+	router.HandleFunc("/ui/search/{txid:[0-9]+}", s.handleSearchSatelliteByID).Methods("GET")
 
-	router.HandleFunc("/data", s.handleSatelliteData).Methods("POST")
+	router.HandleFunc("/data", s.handleSatelliteDataStore).Methods("POST")
 
 	srv := &http.Server{
 		Handler:      router,
